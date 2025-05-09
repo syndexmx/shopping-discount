f46057c8f893f9999f1f382f8f719212b80a5f8f
Maxim Shapovalov <syndex@list.ru>
2025-05-09 19:34:02 +0300

    minor additions to request listener

---
+++ src/main/java/com/github/syndexmx/shopping_discount/config/DiscountRequestReasonConfig.java
@@ -0,0 +1,8 @@
+package com.github.syndexmx.shopping_discount.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DiscountRequestReasonConfig {
+
+}
--- src/main/java/com/github/syndexmx/shopping_discount/controller/kafka/consumer/DiscountRequestListener.java
+++ src/main/java/com/github/syndexmx/shopping_discount/controller/kafka/consumer/DiscountRequestListener.java
@@ -1,16 +1,32 @@
 package com.github.syndexmx.shopping_discount.controller.kafka.consumer;
 
+import com.github.syndexmx.shopping_discount.controller.kafka.enums.DiscountReason;
+import com.github.syndexmx.shopping_discount.service.handler.DiscountRequestHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 @Slf4j
 public class DiscountRequestListener {
 
-        @KafkaListener(topics = "discount-requests", containerFactory = "kafkaListenerContainerFactory")
-        public void consumeDiscountCommand(ConsumerRecord<String, String> record) {
+    private final Map<DiscountReason, DiscountRequestHandler> reasonHandlers;
 
+    @KafkaListener(topics = "discount-requests", containerFactory = "kafkaListenerContainerFactory")
+    public void consumeDiscountRequest(ConsumerRecord<String, String> record) {
+        DiscountReason discountReason = DiscountReason.fromString(record.value());
+
+    }
+
+    private DiscountReason getDiscountReason(ConsumerRecord<String, String> record) {
+        var commandHeader = record.headers().lastHeader("reason");
+        if (commandHeader != null) {
+            return DiscountReason.fromString(new String(commandHeader.value()));
+        } else {
+            return DiscountReason.UNDEFINED;
         }
+    }
 }
