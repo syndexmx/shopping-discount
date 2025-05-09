2f38c6417d0b65a6066e9a1bf02fcdcdfab723b9
Maxim Shapovalov <syndex@list.ru>
2025-05-09 08:16:57 +0300

    Initial commit

---
+++ src/main/java/com/github/syndexmx/shopping_discount/ShoppingDiscountApplication.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.shopping_discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingDiscountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingDiscountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/syndexmx/shopping_discount/ShoppingDiscountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.shopping_discount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingDiscountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
